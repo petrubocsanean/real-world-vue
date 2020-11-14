9cb0e0af16e02d19507e41b36b3cfd64c7e14fd7
Petru Bocsanean <petru.bocsanean@gmail.com>
2020-11-14 14:26:06 +0000

    feat: first commit

---
+++ src/components/EventCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object
+  }
+}
+</script>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  margin-bottom: 18px;
+  border: 1px solid #39495c;
+}
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,53 @@
+<template>
+  <h1>Events for Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+
+export default {
+  name: 'EventList',
+  data() {
+    return {
+      events: [
+        {
+          id: 222,
+          category: 'test event',
+          title: 'test event title',
+          description: 'event description test',
+          location: 'Castelo Branco',
+          date: '02/02/2020',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'John Doe'
+        },
+        {
+          id: 223,
+          category: 'test event 2',
+          title: 'test event title 2',
+          description: 'event description test',
+          location: 'Amsterdam',
+          date: '02/02/2020',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'John Doe'
+        }
+      ]
+    }
+  },
+  components: {
+    EventCard
+  }
+}
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
